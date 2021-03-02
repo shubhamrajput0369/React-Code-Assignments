b880039b842cd4495ac771657cd2475d09fca409
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-02 07:53:29 +0530

    Add files via upload
    Importing Packages and ReactDOM Method added.

---
+++ 2. Importing Packages and ReactDOM Method/index.js
@@ -0,0 +1,8 @@
+//var React = require("react");     //->  Replacing this statement with below adv. javascript using Babel.
+import React from 'react';
+
+//var ReactDOM = require("react-dom");    //->  Replacing this statement with below adv. javascript using Babel. 
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello, World! // (This code demonstrates replacing statements of code with adv. javascript using Babel.)</h1>,
+   document.getElementById("root"));
