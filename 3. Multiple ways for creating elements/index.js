8a00173a795ae4ca34c6e4a48562229e9171a901
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-03 22:00:13 +0530

    Add files via upload
    "Multiple ways for creating elements" & "How to add multiple elements in single render()" concepts added.

---
+++ 3. Multiple ways for creating elements/index.js
@@ -0,0 +1,19 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
+
+//Normal using react method
+
+ReactDOM.render(<h1>Namaskar!</h1>,
+   document.getElementById("root"));
+
+
+//Javascript using Babel
+//   "use strict";
+
+   ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "Namaste!"), document.getElementById("root"));
+
+
+//In Pure Javascript
+   var h1 = document.createElement(h1);
+   h1.innerHTML = "Hello, World!";
+   document.getElementByID("root").appendChild(h1);
---
+++ 4. How to add multiple elements in single render()/index.js
@@ -0,0 +1,20 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
+
+//First Method
+ReactDOM.render(
+   <div>
+      <h1>Namaskar!</h1>
+      <p>How are you?</p>  
+   </div>,
+   document.getElementById("root"));
+
+//or
+
+//Second Method
+ReactDOM.render(
+   [
+      <h1>Namaskars!</h1>,
+      <p>How are you all?</p>
+   ],  
+   document.getElementById("root"));
