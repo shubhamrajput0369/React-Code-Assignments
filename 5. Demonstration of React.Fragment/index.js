aa010d1b96f9d83a579862f361e283d8686faadc
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-04 21:37:32 +0530

    Add files via upload
    Demonstration of React.Fragment, Revision - Challenge 1 React files added.

---
+++ 5. Demonstration of React.Fragment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+   <React.Fragment>
+      <h1>Namaskar!</h1>
+      <p>How are you?</p>  
+      <h6>Thank You!</h6>
+   </React.Fragment>,
+   document.getElementById("root"));
---
+++ 6. Revision - Challenge 1 React/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+   <React.Fragment>
+      <h1>Shubham's Favourite Netflix Web Series</h1>
+      <p>List of 5 Best Series</p>
+      <ol>
+         <li>SpecialOpps</li>  
+         <li>Hackerness</li>
+         <li>TechInception</li>
+         <li>Inception of Universe</li>
+         <li>Brain Wish</li>
+      </ol>
+   </React.Fragment>,
+   document.getElementById("root"));
