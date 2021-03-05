1b3b5dd1ed86ffc9e888616d29ec1bb80e0b5d27
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-05 23:11:46 +0530

    Add files via upload
    "JavaScript Expressions in JSX in ReactJS" and "ES6 Template Literals in JSX in ReactJS" added.

---
+++ 7. JavaScript Expressions in JSX in ReactJS/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const flname = "Shubham Rajput";
+
+ReactDOM.render(
+   <>
+   <h1>Hello, World! My name is {flname}</h1>
+   <p>Sum of 5+5 is {5+5}</p>
+   </>,
+   document.getElementById("root")
+   );
---
+++ 8. ES6 Template Literals in JSX in ReactJS/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fname = "Shubham";
+const lname = "Rajput";
+
+ReactDOM.render(
+   <>
+   <h1>{`Hello, World! My first name is ${fname} and last name is ${lname}.`}</h1>
+   <p>{`Sum of 5+5 is ${5+5}`}</p>
+   </>,
+   document.getElementById("root")
+   );
